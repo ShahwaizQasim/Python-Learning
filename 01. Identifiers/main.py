5e023c28d8a7bae161270337f3e05b24a9320b26
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-13 04:50:57 -0800

    Identifiers add

---
+++ 01. Identifiers/main.py
@@ -0,0 +1,23 @@
+# python first program run
+print("hello world")
+
+
+age = 22
+print("age=>", age)
+
+name = "Shahwaiz Qasim"
+print("MyName", name)
+
+
+# valid identifiers
+age = 24
+first_name = "Syed Shahwaiz"
+_id = 10243
+name2 = "Muhammad Ali"
+
+# Invalid Identifiers 
+'''
+class = "ali"
+2name = "wasid"
+mark$ = 22
+'''
