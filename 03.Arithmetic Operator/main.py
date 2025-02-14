1cb92b0ba51b95ce546ba90876566009ba8d656e
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-14 04:51:03 -0800

    operators learning

---
+++ 03.Arithmetic Operator/main.py
@@ -0,0 +1,22 @@
+# Arithmetic Operators
+a = 4
+b = 5
+
+
+print("Plus", a + b)
+print("Minus", a - b)
+print("Multiply", a * b)
+print("4 ki power 5", a ** b)
+print("Divide", a / b)
+
+# 4 ÷ 5 = 0.8, lekin floor division sirf integer part (0) return karega.
+print(a//b) # 4 // 5 ka matlab hai 4 ko 5 se divide karo aur integer part return karo.
+
+print("Modulus", a % b)
+
+# Average Three Numbers
+a = 4
+b = 3
+c = 5
+
+print("Average three numbers", a + b + c / 3)
