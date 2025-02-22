e7a1a9fcbf36356792f0a7fcf979043af5b23868
ShahwaizQasim <syedshahwaiz32@gmail.com>
2025-02-21 23:01:12 -0800

    conditional statements

---
+++ 05.Conditional_Statements/main.py
@@ -0,0 +1,27 @@
+# if else syntax
+# if condition:
+#     print()
+# else:
+#     print()
+
+
+age = 12
+if age > 18:
+    print("you can vote")
+else:
+    print("you can not vote")
+
+print("This will always be execute")
+
+
+# if elif else syntax 
+
+num = 0
+
+if num > 0:
+    print("Positive Number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative Number")
+
